876b936410048d639d26ca915e525466e22de055
JC <jdmc2@jdmc2.com>
2014-11-11 07:39:21 +0000

    updated switchoff.py so it doesn't log heavily.

--- switchoff.py
+++ switchoff.py
@@ -9,12 +9,14 @@ GPIO.setup(POWERPIN, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
 print "Starting shutdown GPIO monitor"
 while (GPIO.input(POWERPIN) == True):
-#    print "pin is high - doing nothing."
     time.sleep(1)
+
+# if we fall through the above, the pin has been pushed!
 
 print "switchoff.py: detected pin is high - so rebooting... "
 os.system("echo mmc0 > /sys/class/leds/led0/trigger")
-os.system("sudo shutdown -h now&")
+os.system("sudo shutdown -h now &")
 
 while True:
   print "rebooting..."
+  time.sleep(1)

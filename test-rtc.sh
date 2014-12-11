c664fe6eba443091950075f71a0922878f16bf97
JC <jdmc2@jdmc2.com>
2014-12-11 12:58:49 +0000

    RTC work

--- test-rtc.sh
+++ test-rtc.sh
@@ -4,12 +4,12 @@ echo
 echo 
 echo "*** This is test-RTC - tests the operation, and sets the RTC"
 echo
-echo "Current time is :"
+echo "Current system time is :"
 date
 
 echo "Looking for RTC... ( checking /dev/rtc0)"
 
-if [ ! -f /dev/rtc0 ] ; then
+if [ ! -c /dev/rtc0 ] ; then
     echo "No such file - /dev/rtc0"
     echo "exiting..."
     exit -1

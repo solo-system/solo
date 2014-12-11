c2cdab06483c269da69f1b784d47f02e2277836d
JC <jdmc2@jdmc2.com>
2014-12-11 19:19:48 +0000

    rtc clock stuff

--- test-rtc.sh
+++ test-rtc.sh
@@ -23,5 +23,33 @@ echo "sysdate: $sysdate"
 echo "rtcdate: $rtcdate"
 echo "netdate: $netdate"
 
+echo "Run with net2sys or sys2rtc (or both) if desired"
+
+if [ $1 == "net2sys" ] ; then
+    echo "setting system time from net ..."
+    rdate time.nist.gov
+    echo "Done - time is now `date`"
+    shift
+fi
+
+if [ $1 == "sys2rtc" ] ; then
+    echo "setting rtc from system time ..."
+    hwclock --systohc
+    echo "Done."
+    shift
+fi
+
+echo
+echo "times just before we exit:"    
+sysdate=`date`
+rtcdate=`/sbin/hwclock --show`
+netdate=`rdate -p time.nist.gov`
+
+echo "sysdate: $sysdate"
+echo "rtcdate: $rtcdate"
+echo "netdate: $netdate"
+
+echo "done"
+echo
 
 exit 0

e5544134fdbd47665821c92e596e5e551eb04182
JC - james <test@test.com>
2015-02-22 18:50:24 +0000

    fixed update to jdmc2.com/git/solo.git from .../git/solo

--- provision.sh
+++ provision.sh
@@ -61,7 +61,7 @@ echo
 echo "Preparing our boot scripts"
 chmod +x /opt/solo/solo-boot.sh /opt/solo/switchoff.py
 echo "Downloading and Installing amon ..."
-( cd /home/amon/ ; git clone jdmc2@jdmc2.com:git/amon )
+( cd /home/amon/ ; git clone jdmc2@jdmc2.com:git/amon.git )
 chown -R amon.amon /home/amon
 chmod +x /home/amon/amon/amon # gosh - that's silly
 echo "PATH=$PATH:/home/amon/amon/" > /home/amon/.bashrc

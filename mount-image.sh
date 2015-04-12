d86c1fb8d6dadd95091d360271a5df1b928b9038
James Christie <jc@sanaga.com>
2015-04-12 14:37:11 +0100

    tidy up

--- mount-image.sh
+++ mount-image.sh
@@ -40,31 +40,3 @@ echo "sudo umount p1 ; sudo losetup -d /dev/loop0 ; rmdir p1"
 echo 
 
 exit 0
-
-
-### resize stuff is below
-originally, df said 2721336 x 1k blocks  AND 707840 (blocks) from e2fsck.
-resize says: Resizing the filesystem on /dev/loop0 to 498264 (4k) blocks.
-e2fsck now says 498264 4k blocks.  (mount, then df and get = 1895152 1k blocks 
-and indeed 
-1993056 - 1895152
-
-sudo e2fsck -f /dev/loop0
-sudo resize2fs -M /dev/loop0  (again and again if you like!)
-> currently get fs as 269186 blocks long (bloks are 4k).  So to add 100Mb, add 25,000 blocks)
-sudo resize2fs -p /dev/loop0 $((269186+25000))
-sudo losetup -d /dev/loop0
-sudo sync; 
-
-> shrink containing partition: 294186 *4 (to get K).
-onekblocks=`echo "294186 * 4" | bc`
-fcmd="d \n 2 \n n \n p \n 2 \n 122880 \n +${onekblocks}K \n w"
-echo -e $fcmd
-echo "Now run: echo -e \$fcmd | fdisk whichever.img"
-
-> Then check truncating the img file itself.
-> fdisk says end of p2 is 3322880, and blocks are 512bytes, so truncate:
-truncate -s $((3322880*512)) dd.img
-zip -r image.zip image.img
-
-done

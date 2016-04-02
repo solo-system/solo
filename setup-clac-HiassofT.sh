c68c6ee1ff35007377bc212f229334fc03b19a66
jdmc2 <jdmc2@hp>
2016-04-02 09:35:55 +0100

    improved Hiassoft install script.

--- setup-clac-HiassofT.sh
+++ setup-clac-HiassofT.sh
@@ -1,9 +1,14 @@
 # this installs CLAC support according to HiassofT's instructions
 # it supersedes RJ's in setup-clac.sh
 
-# TODO: could save space removing all the old /lib/modules/{3.18.11+,3.18.11-v7+}
-# .. whichever are NOT this kernel's modules.
-# and the old kernels too, of course.
+# TODO: remove unused kernels and /lib/modules: rm -rf
+# /lib/modules/{3.18.11+,3.18.11-v7+} .. whichever are NOT this
+# kernel's modules. (but which is that?)  Tried this: origmodules=$(ls
+# /lib/modules) - but still need to take care that HiassofT's aren't
+# the same version number - so need to do a wc -l on /lib/modules
+# later to see if there is > 2 (one for arm6 one for arm7) - only then
+# can we rm -rf $origmoldules.  Too excited to do this cleanup work
+# right now - want to try tem out.
 
 echo "Installing Support for HiassofT's CLAC..."
 
@@ -12,11 +17,11 @@ mkdir $WORKDIR
 pushd $WORKDIR
 
 echo "... fetching [ my copy of ] HiassofT's tar file"
-wget jdmc2.com/solo/HiassofT/cirrus-linux-4.1.19.tgz
+wget jdmc2.com/solo/HiassofT/cirrus-linux-latest.tgz
 wget jdmc2.com/solo/HiassofT/usecase-scripts.tgz
 
 echo "... and untarring the kernel files"
-tar zxf cirrus-linux-4.1.18.tgz -C /
+tar zxf cirrus-linux-latest.tgz -C /
 
 echo "... and now untarring the use-case scripts"
 mkdir -p /home/amon/clac
@@ -26,23 +31,26 @@ chmod +x /home/amon/clac/*.sh
 
 popd # go back to where we were (wherever that was - I don't remember why this was important)
 
-# see http://www.horus.com/~hias/cirrus-driver.html
+# 
 echo "... Updating /boot/config.txt"
 cp /boot/config.txt /boot/config.txt.pre-provision
 cat <<EOF >> /boot/config.txt
-# Below added by solo's setup-clac-HiassofT.sh
+
+# Below lines have been added by solo's setup-clac-HiassofT.sh
+# see http://www.horus.com/~hias/cirrus-driver.html for details
 # Add the following line to /boot/config.txt to enable the Cirrus Logic card driver
 dtoverlay=rpi-cirrus-wm5102
 # If you want MMAP support (eg for Alsa plugins) add this line as well:
 dtoverlay=i2s-mmap
 EOF
 
-
 # Setup module dependencies
 # The cirrus driver requires some manually defined module dependencies,
 # if you fail to add them the driver won't load properly.
 # Create a file /etc/modprobe.d/cirrus.conf with the following content:
-cat << EOF > /etc/modprobe.d/cirrus.conf
+cat <<EOF > /etc/modprobe.d/cirrus.conf
+# Below lines have been added by solo's setup-clac-HiassofT.sh
+# see http://www.horus.com/~hias/cirrus-driver.html for details
 softdep arizona-spi pre: arizona-ldo1
 softdep spi-bcm2708 pre: fixed
 softdep spi-bcm2835 pre: fixed

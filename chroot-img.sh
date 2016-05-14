02e9e1e7b76174fdf6e83d359fd74a3530ed2f63
JC <jdmc2@pcs>
2016-05-14 12:31:04 +0100

    should we run pre-provision.sh or not...  Command line arg probably later.

--- chroot-img.sh
+++ chroot-img.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# mount a "img" and run commands within a chrooted quemu-user-static environment"
+# mount "img" and run commands within a chrooted quemu-user-static environment"
 # WARNING - this works for me.  Use at your own risk.
 
 function die(){
@@ -13,13 +13,13 @@ function mount_image() {
 	echo "Error: mount_image() requires 2 arguments - Exiting (got $*)"
 	exit -1
     fi
-    
+
     img=$1
     if [ ! -w $img ] ; then
 	echo "Error: mount_image(): no such writable image file: $img. Exiting."
 	exit -1
     fi
-    
+
     dir=$2
     if [ -e $dir ] ; then
 	echo "Error: mount_image(): output $dir already exists - refusing to overwrite. Exiting."
@@ -33,7 +33,7 @@ function mount_image() {
     mkdir $dir
     sudo mount $img -o loop,offset=$((512*p2offset)) $dir/
     sudo mount $img -o loop,offset=$((512*p1offset)) $dir/boot/
-    
+
     echo "mount_image(): mounted \"$img\" on \"$dir\" offsets: [p1,$p1offset] [p2,$p2offset]"
     sleep 1 # let things settle (don't run umount_image immediately, it fails).
 }
@@ -43,7 +43,7 @@ function umount_image() {
 	echo "Error: mount_image() requires 1 argument (directory) - Exiting (got $*)"
 	exit -1
     fi
-    
+
     dir=$1
     if [ ! -e $dir ] ; then
 	echo "Error: mount_image(): no such directory $dir. Exiting."
@@ -53,7 +53,7 @@ function umount_image() {
     sudo umount $dir/boot/
     sudo umount $dir/
     sync
-    rmdir vroot    
+    rmdir vroot
     echo "umount_image(): unmounted both partitions and removed dir \"$dir\"."
 }
 
@@ -75,8 +75,8 @@ sudo cp -v /usr/bin/qemu-arm-static vroot/usr/bin/
 sudo cp -v /home/jdmc2/git/solo/pre-provision.sh vroot/opt/
 
 echo "Starting chroot..."
-#sudo chroot vroot /bin/bash /opt/pre-provision.sh
-sudo chroot vroot /bin/bash 
+sudo chroot vroot /bin/bash /opt/pre-provision.sh
+#sudo chroot vroot /bin/bash
 echo "... Chroot closed."
 
 # undo the things we needed

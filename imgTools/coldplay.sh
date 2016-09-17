598da9ec5d3ef90dca6a4b0493f7ad6527996533
JC <info@solo-system.org>
2016-09-17 18:37:19 +0100

    added functionality to copy all data onto host.

--- imgTools/coldplay.sh
+++ imgTools/coldplay.sh
@@ -1,27 +1,35 @@
 #!/bin/bash
 
-# travis CI for solo - so it's called coldplay
+# travisCI for solo.
+# It gets some high level results from a full SD-card
+# and prints essential debug.
+# it now optionally copies the data to somewhere too.
 
 if [ ! $1 ] ; then
     echo "Usage: coldplay.sh /dev/sdX"
     exit -1
 fi
-
 dev=$1
+echo "Using device: $dev"
+shift
 
-if mount | grep sdh ; then
+savedir=""
+if [ $1 ] ; then
+    savedir=/mnt/a/solo/harvested/coldplay/$1
+    echo "Will save data to directory \"$savedir\""
+fi
+
+# Check none of the partitions are already mounted...
+if mount | grep $dev ; then
     echo ERROR: something is mounted already.
-    echo Try: sudo umount /dev/$dev\*
+    echo Try: sudo umount $dev\*
     exit -1
 fi
 
 tmpdir=$(mktemp -u -d /tmp/coldplay.XXXXXX)
 
 echo "Using tmpdir=$tmpdir"
-mkdir $tmpdir
-
-mkdir $tmpdir/p1 $tmpdir/p2 $tmpdir/p3
-
+mkdir -p $tmpdir/{p1,p2,p3}
 sudo mount ${dev}1 $tmpdir/p1
 sudo mount ${dev}2 $tmpdir/p2
 sudo mount ${dev}3 $tmpdir/p3
@@ -38,14 +46,23 @@ numwavs=$(find $tmpdir/p3/amondata/wavs/ -name \*.wav -ls | wc -l)
 sizewavs=$(find $tmpdir/p3/amondata/wavs/ -name \*.wav -ls | awk '{ sum+=$7 } END {print sum / 1000000}')
 echo Recorded $numwavs different wav files totalling $sizewavs MB of audio.
 
-echo "Timezone information:"
 grep "Current default time zone" $tmpdir/p2/opt/solo/solo-boot.log
 
-echo "arecord.log:"
+#echo "arecord.log:"
 grep "Recording WAVE" $tmpdir/p3/amondata/logs/arecord.log
 
 #echo WAITING
 #read
+if [ $savedir ] ; then
+    
+    if [  -d $savedir ] ; then
+	echo "WARNING: savedir $savedir already exists - not copying"
+    else
+	mkdir -pv $savedir
+	echo "copying data from $tmpdir/p3 to $savedir"
+	cp -prv $tmpdir/p3/amondata $savedir
+    fi
+fi
 
 sudo umount $tmpdir/p1
 sudo umount $tmpdir/p2

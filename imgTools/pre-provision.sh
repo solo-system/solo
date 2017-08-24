862fc2a7d71a8f0dcc8f728139e64ff6a5fcd122
JC <info@solo-system.org>
2017-08-24 17:22:25 +0100

    tidyups after a long dev session.

--- imgTools/pre-provision.sh
+++ imgTools/pre-provision.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
 
-# provision.sh used to be run on physical raspi hardware, but not any
-# more.
+# show what we are doing.
+set -x
+
+# you can't merge this with provision.sh, cos you need to clone
+# solo.git before you can run provision.sh.  Needs 2 stages.
 
 # pre-provision used to be run by hand (on the pi) to do things like
 # install git, which was needed to then download provision.sh.  It
 # became a script, and now that we do it all in qemu, it should really
 # be merged.
 
-# Actually, installing git is not the only thing we need.  the default
-# resize2fs of p1 at boot time throug /etc/init.d/resize2fs_once
-# renders hardware provisioning pretty difficult.
-
-# TODO: move all this into provision.sh - we aren't going back to the
-# hardware days.
-
-echo
 echo "hello - I am the pre-provisioner.  I run things inside the chroot, prior to handing over to provision.sh"
-echo "previously, provision.sh was runnable on a hardware (real) pi, and these two haven't been properly merged now that we do everyting in chroot/qemu"
-echo
 
 # raspbian lite does NOT have git, and a few other little things.
 echo "do a apt-get update and apt-get install -y git."

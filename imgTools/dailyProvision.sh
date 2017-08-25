a4c2e505bebc5b5732232086a298b7f2640c0288
JC <info@solo-system.org>
2017-08-25 18:31:04 +0100

    update logging in dailyProvision.sh

--- imgTools/dailyProvision.sh
+++ imgTools/dailyProvision.sh
@@ -32,7 +32,9 @@ echo "running: $bin/img-chroot.sh $bin/pre-provision.sh"
 cp $srcimg ./copy.img
 $bin/img-chroot.sh ./copy.img $bin/pre-provision.sh >& dailyProvision.log
 
-echo "dailyProvision: Finished pre-provision.sh. Now shrinking ..."
+echo "dailyProvision: Finished pre-provision.sh"
+
+echo "Now shrinking ..."
 cp -v copy.img shrunk.img
 $bin/img-shrink.sh -f shrunk.img >> dailyProvision.log 2>&1
 echo "dailyProvision: Done shrinking"
@@ -42,25 +44,25 @@ sudo chown -R jdmc2.jdmc2  $workdir
 sudo chmod u+w $workdir/*
 mv -v shrunk.img sosi-$daystamp.img
 
+zip sosi-$daystamp.img.zip sosi-$daystamp.img
+
 echo "------------------------------------------------------------"
 echo "Done - here's some helpful info for a release:"
-zip sosi-$daystamp.img.zip sosi-$daystamp.img
 
 sha=$(sha1sum sosi-$daystamp.img.zip)
 size=$(stat --format "%s" sosi-$daystamp.img.zip)
 sizeMB=$(echo " $size / (1024*1024) " | bc)
-
 solohead=$(cd /home/jdmc2/git/solo ; git rev-parse HEAD)
 amonhead=$(cd /home/jdmc2/git/amon ; git rev-parse HEAD)
 
 echo "--------------------------------"
-echo "  name: sosi-$daystamp.img"
+echo "  name       : sosi-$daystamp.img"
+echo "  raspbian   :  This SOSI is based on $(basename $srcimg)"
 echo "  sha1sum    : $sha"
 echo "  size       : ${sizeMB}MB"
 echo "  exact size : $size"
-echo "  solo: git rev-parse HEAD: $solohead"
-echo "  amon: git rev-parse HEAD: $amonhead"
-echo "  raspbian:  This SOSI is based on $(basename $srcimg)"
+echo "  solo       : git rev-parse HEAD: $solohead"
+echo "  amon       : git rev-parse HEAD: $amonhead"
 echo "  This release includes:"
 echo "    These things XXXXX"
 echo "--------------------------------"

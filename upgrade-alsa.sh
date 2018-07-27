515e6e5c309cbb59c29fd50b1f9697c0627f4f52
JC <john.doe@example.com>
2018-07-27 12:34:44 +0100

    added commentary to end of upgrade-alsa.sh

--- upgrade-alsa.sh
+++ upgrade-alsa.sh
@@ -45,3 +45,18 @@ $localalsadir/installdir/bin/arecord --version
 rm -rf alsa-lib-1.1.6 alsa-lib-1.1.6.tar.bz2 alsa-utils-1.1.6 alsa-utils-1.1.6.tar.bz2 
 
 echo "install-alsa.sh Exiting."
+
+# NOTES: The rm above removes all the source directories.  The
+# "installdir" left over, is everything that the two alsa modules
+# (-lib and -utils) wanted to install. We get away with just adding
+# /opt/upgrade-alsa/installdir/bin to amon's path (for access to the
+# -utils tools - including arecord - the most important), but there
+# are a few other things in "installdir" that are noteworthy...
+
+# udevrulesdir - should (but doesn't) overwrite /lib/udev/rules.d/90-alsa-restore.rules
+#  - this is to do with udev. Not much different from stock one that comes with default alsa install.  So I'm leaving it uninstalled.
+
+# systemdfiles - again, without really exploring, I am leaving these here without installing them to the system.
+# I suppose it's bad to have 2 versions of alsa lying around - particularly when the "system" is using old-alsa to do system things (like restore etc...) and I am using new-alsa, but they are close enough in versions (1.1.3 versus 1.1.6 that it hopefully won't matter).
+
+# written on 2018-07-27.  Looking forward to future versions of raspbian where all this can go away, because stock version of alsa-utils will be 1.1.6. Looks like that's happening in "buster".

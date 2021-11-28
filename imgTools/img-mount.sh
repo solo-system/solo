c16574d728850e71245208e692f9ee4463f47871
JC <info@solo-system.org>
2021-11-28 10:35:16 +0000

    typo

--- imgTools/img-mount.sh
+++ imgTools/img-mount.sh
@@ -15,7 +15,7 @@
 [ $# -eq 1 ] || die "error usage is \"$0 img\""
 img=$1
 # allow it to be a file or a block device (added 2021-11-27)
-[ -b $img -o -f $img] || die "Error: [img-mount] no such image: $img"
+[ -b $img -o -f $img ] || die "Error: [img-mount] no such image: $img"
 
 dir=vroot
 [ -e $dir ] && die "Error: directory $dir exists.  Refusing to overwrite."
